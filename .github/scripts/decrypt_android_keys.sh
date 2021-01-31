5f59f30563c8b3e03ed548884aee14ae4a2532cb
ModernChocolate <modernchocolate@users.noreply.github.com>
2021-01-31 18:43:17 +0100

    Update properties

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -3,4 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
---output android/app/android_keys.zip android/app/android_keys.zip.gpg && cd android/app && jar xvf android_keys.zip && cd -
+--output android/android_keys.zip android/android_keys.zip.gpg && cd android && jar xvf android_keys.zip && cd -
