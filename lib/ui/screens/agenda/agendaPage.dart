9f86673c3a4cd21cdd9b0c55460fd8e78ca25083
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-06-11 17:33:00 +0200

    migrate agenda to new router

--- lib/ui/screens/agenda/agendaPage.dart
+++ lib/ui/screens/agenda/agendaPage.dart
@@ -6,6 +6,9 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 import 'package:ynotes/core/utils/themeUtils.dart';
 import 'package:ynotes/ui/components/expandables.dart';
 import 'package:ynotes/ui/components/hiddenSettings.dart';
+import 'package:ynotes/ui/components/y_page/mixins.dart';
+import 'package:ynotes/ui/components/y_page/y_page.dart';
+import 'package:ynotes/ui/components/y_page/y_page_local.dart';
 import 'package:ynotes/ui/screens/agenda/agendaPageWidgets/agenda.dart';
 import 'package:ynotes/ui/screens/agenda/agendaPageWidgets/agendaSettings.dart';
 import 'package:ynotes/ui/screens/agenda/agendaPageWidgets/spaceAgenda.dart';
@@ -19,7 +22,7 @@ class AgendaPage extends StatefulWidget {
   AgendaPageState createState() => AgendaPageState();
 }
 
-class AgendaPageState extends State<AgendaPage> {
+class AgendaPageState extends State<AgendaPage> with YPageMixin {
   PageController agendaPageSettingsController = PageController(initialPage: 1);
   double btPercents = 0;
   double topPercents = 100;
@@ -27,6 +30,26 @@ class AgendaPageState extends State<AgendaPage> {
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context);
+    return YPage(
+      title: "Agenda",
+      body: Expandables(
+        buildTopChild(),
+        buildBottomChild(),
+        width: screenSize.size.width,
+        maxHeight: screenSize.size.height / 10 * 7.2,
+        minHeight: screenSize.size.height / 10 * 0.7,
+        bottomExpandableColor: ThemeUtils.spaceColor(),
+        onDragUpdate: handleDragUpdate,
+        animationDuration: 200,
+        topExpandableBorderRadius: 11,
+        bottomExpandableBorderRadius: 11,
+      ),
+      actions: [
+        IconButton(
+            onPressed: () => openLocalPage(YPageLocal(title: "Options", child: AgendaSettings())),
+            icon: Icon(MdiIcons.wrench))
+      ],
+    );
     return Scaffold(
       appBar: new AppBar(
           title: new Text(
@@ -34,17 +57,13 @@ class AgendaPageState extends State<AgendaPage> {
             style: TextStyle(fontFamily: "Asap", fontWeight: FontWeight.bold),
           ),
           leading: TextButton(
-            style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.transparent)),
+            style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent)),
             child: Icon(MdiIcons.menu, color: ThemeUtils.textColor()),
             onPressed: () {},
           ),
           actions: [
             TextButton(
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.transparent)),
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent)),
               child: Icon(MdiIcons.tuneVariant, color: ThemeUtils.textColor()),
               onPressed: () async {
                 triggerSettings();
@@ -79,34 +98,26 @@ class AgendaPageState extends State<AgendaPage> {
       child: Stack(
         children: [
           Transform.translate(
-            offset: Offset(
-                0, -(btPercents / 100) * screenSize.size.height / 10 * 0.7),
+            offset: Offset(0, -(btPercents / 100) * screenSize.size.height / 10 * 0.7),
             child: Container(
               height: screenSize.size.height / 10 * 0.7,
               width: screenSize.size.width,
-              decoration: BoxDecoration(
-                  color: Color(0xff100A30),
-                  borderRadius: BorderRadius.circular(0)),
+              decoration: BoxDecoration(color: Color(0xff100A30), borderRadius: BorderRadius.circular(0)),
               child: Stack(
                 children: [
                   Align(
                     alignment: Alignment.centerLeft,
                     child: Container(
-                        margin: EdgeInsets.only(
-                            left: screenSize.size.width / 5 * 0.1),
+                        margin: EdgeInsets.only(left: screenSize.size.width / 5 * 0.1),
                         child: AutoSizeText(
                           "Organisation extra-scolaire",
-                          style: TextStyle(
-                              fontFamily: "Asap",
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
+                          style: TextStyle(fontFamily: "Asap", color: Colors.white, fontWeight: FontWeight.bold),
                         )),
                   ),
                   Align(
                     alignment: Alignment.centerRight,
                     child: Container(
-                      margin: EdgeInsets.only(
-                          right: screenSize.size.width / 5 * 0.1),
+                      margin: EdgeInsets.only(right: screenSize.size.width / 5 * 0.1),
                       child: Transform.rotate(
                         angle: pi * (btPercents / 100),
                         child: Icon(
@@ -136,34 +147,27 @@ class AgendaPageState extends State<AgendaPage> {
       child: Stack(
         children: [
           Transform.translate(
-            offset: Offset(
-                0, -(topPercents / 100) * screenSize.size.height / 10 * 0.7),
+            offset: Offset(0, -(topPercents / 100) * screenSize.size.height / 10 * 0.7),
             child: Container(
               height: screenSize.size.height / 10 * 0.7,
               width: screenSize.size.width,
-              decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(0)),
+              decoration: BoxDecoration(color: Theme.of(context).primaryColor, borderRadius: BorderRadius.circular(0)),
               child: Stack(
                 children: [
                   Align(
                     alignment: Alignment.centerLeft,
                     child: Container(
-                        margin: EdgeInsets.only(
-                            left: screenSize.size.width / 5 * 0.1),
+                        margin: EdgeInsets.only(left: screenSize.size.width / 5 * 0.1),
                         child: AutoSizeText(
                           "Agenda",
-                          style: TextStyle(
-                              fontFamily: "Asap",
-                              color: ThemeUtils.textColor(),
-                              fontWeight: FontWeight.bold),
+                          style:
+                              TextStyle(fontFamily: "Asap", color: ThemeUtils.textColor(), fontWeight: FontWeight.bold),
                         )),
                   ),
                   Align(
                     alignment: Alignment.centerRight,
                     child: Container(
-                      margin: EdgeInsets.only(
-                          right: screenSize.size.width / 5 * 0.1),
+                      margin: EdgeInsets.only(right: screenSize.size.width / 5 * 0.1),
                       child: Transform.rotate(
                         angle: pi * (topPercents / 100),
                         child: Icon(
@@ -194,9 +198,7 @@ class AgendaPageState extends State<AgendaPage> {
   }
 
   void triggerSettings() {
-    agendaPageSettingsController.animateToPage(
-        agendaPageSettingsController.page == 1 ? 0 : 1,
-        duration: Duration(milliseconds: 300),
-        curve: Curves.ease);
+    agendaPageSettingsController.animateToPage(agendaPageSettingsController.page == 1 ? 0 : 1,
+        duration: Duration(milliseconds: 300), curve: Curves.ease);
   }
 }
