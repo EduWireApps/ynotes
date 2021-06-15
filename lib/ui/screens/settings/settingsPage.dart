aed574172e3c7493189fc9504582e0812b678a0b
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-06-15 16:09:18 +0200

    migrate accountPage to new router

--- lib/ui/screens/settings/settingsPage.dart
+++ lib/ui/screens/settings/settingsPage.dart
@@ -20,6 +20,8 @@ import 'package:ynotes/core/utils/settingsUtils.dart';
 import 'package:ynotes/core/utils/themeUtils.dart';
 import 'package:ynotes/globals.dart';
 import 'package:ynotes/ui/components/dialogs.dart';
+import 'package:ynotes/ui/components/y_page/mixins.dart';
+import 'package:ynotes/ui/components/y_page/y_page_local.dart';
 import 'package:ynotes/ui/screens/settings/sub_pages/accountPage.dart';
 import 'package:ynotes/ui/screens/settings/sub_pages/logsPage.dart';
 
@@ -39,7 +41,7 @@ class SettingsPage extends StatefulWidget {
   }
 }
 
-class _SettingsPageState extends State<SettingsPage> with TickerProviderStateMixin {
+class _SettingsPageState extends State<SettingsPage> with TickerProviderStateMixin, YPageMixin {
   late AnimationController leftToRightAnimation;
   late AnimationController rightToLeftAnimation;
   //Avatar's animations :
@@ -82,9 +84,7 @@ class _SettingsPageState extends State<SettingsPage> with TickerProviderStateMix
                       subtitle: '${appSys.currentSchoolAccount?.name ?? "Invité"}',
                       leading: Icon(MdiIcons.account, color: ThemeUtils.textColor()),
                       trailing: Icon(Icons.chevron_right, color: ThemeUtils.textColor()),
-                      onPressed: (context) {
-                        Navigator.of(context).push(router(AccountPage()));
-                      },
+                      onPressed: (context) => openLocalPage(YPageLocal(child: AccountPage(), title: "Compte")),
                       iosChevron: Icon(Icons.chevron_right)),
                 ],
               ),
