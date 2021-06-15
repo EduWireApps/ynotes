ef951099027a5804c9627f2d006f58523051fbe3
ModernChocolate <modernchocolate@users.noreply.github.com>
2021-06-15 12:05:49 +0200

    Fix connection status

--- lib/ui/components/y_drawer/widgets/connection_status.dart
+++ lib/ui/components/y_drawer/widgets/connection_status.dart
@@ -8,9 +8,7 @@ class ConnectionStatus extends StatefulWidget {
   final LoginController con;
   final Animation<double> showLoginControllerStatus;
 
-  const ConnectionStatus(
-      {Key? key, required this.con, required this.showLoginControllerStatus})
-      : super(key: key);
+  const ConnectionStatus({Key? key, required this.con, required this.showLoginControllerStatus}) : super(key: key);
 
   @override
   _ConnectionStatusState createState() => _ConnectionStatusState();
@@ -35,58 +33,54 @@ class _ConnectionStatusState extends State<ConnectionStatus> {
                   loginStatus.error: Color(0xffF87171),
                   loginStatus.offline: Color(0xffFCD34D),
                 }),
-                height: screenSize.size.height /
-                    10 *
-                    0.4 *
-                    (1 - widget.showLoginControllerStatus.value),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ClipRRect(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                          case2(
-                            widget.con.actualState,
-                            {
-                              loginStatus.loggedOff: SpinKitThreeBounce(
-                                size: screenSize.size.width / 5 * 0.3,
-                                color: Color(0xff57534E),
-                              ),
-                              loginStatus.offline: Icon(
-                                MdiIcons.networkStrengthOff,
-                                size: screenSize.size.width / 5 * 0.3,
-                                color: Color(0xff78716C),
-                              ),
-                              loginStatus.error: GestureDetector(
-                                onTap: () async {},
-                                child: Icon(
-                                  MdiIcons.exclamation,
-                                  size: screenSize.size.width / 5 * 0.3,
+                height: (screenSize.size.height / 10 * 0.4 * (1 - widget.showLoginControllerStatus.value)),
+                child: ClipRRect(
+                  child: Wrap(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+                            case2(
+                              widget.con.actualState,
+                              {
+                                loginStatus.loggedOff: SpinKitThreeBounce(
+                                  size: 30,
                                   color: Color(0xff57534E),
                                 ),
-                              ),
-                              loginStatus.loggedIn: Icon(
-                                MdiIcons.check,
-                                size: screenSize.size.width / 5 * 0.3,
+                                loginStatus.offline: Icon(
+                                  MdiIcons.networkStrengthOff,
+                                  size: 30,
+                                  color: Color(0xff78716C),
+                                ),
+                                loginStatus.error: GestureDetector(
+                                  onTap: () async {},
+                                  child: Icon(
+                                    MdiIcons.exclamation,
+                                    size: 30,
+                                    color: Color(0xff57534E),
+                                  ),
+                                ),
+                                loginStatus.loggedIn: Icon(
+                                  MdiIcons.check,
+                                  size: 30,
+                                  color: Color(0xff57534E),
+                                )
+                              },
+                              SpinKitThreeBounce(
+                                size: 30,
                                 color: Color(0xff57534E),
-                              )
-                            },
-                            SpinKitThreeBounce(
-                              size: screenSize.size.width / 5 * 0.4,
-                              color: Color(0xff57534E),
-                            ),
-                          ) as Widget,
-                        ])),
-                    Text(widget.con.details,
-                        style: TextStyle(
-                            fontFamily: "Asap", color: Color(0xff57534E))),
-                    Text(" Voir l'état du compte.",
-                        style: TextStyle(
-                            fontFamily: "Asap",
-                            color: Color(0xff57534E),
-                            fontWeight: FontWeight.bold))
-                  ],
+                              ),
+                            ) as Widget,
+                          ]),
+                          Text(widget.con.details, style: TextStyle(fontFamily: "Asap", color: Color(0xff57534E))),
+                          Text(" Voir l'état du compte.",
+                              style:
+                                  TextStyle(fontFamily: "Asap", color: Color(0xff57534E), fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
