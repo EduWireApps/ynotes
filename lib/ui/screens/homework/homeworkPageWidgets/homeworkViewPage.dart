d8dc5bca09873bc18579e2c4948835d5202a7a54
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-06-29 16:34:26 +0200

    move all extensions to lib/extensions.dart

--- lib/ui/screens/homework/homeworkPageWidgets/homeworkViewPage.dart
+++ lib/ui/screens/homework/homeworkPageWidgets/homeworkViewPage.dart
@@ -13,6 +13,7 @@ import 'package:ynotes/core/logic/modelsExporter.dart';
 import 'package:ynotes/core/offline/data/homework/homework.dart';
 import 'package:ynotes/core/utils/themeUtils.dart';
 import 'package:ynotes/globals.dart';
+import 'package:ynotes/extensions.dart';
 import 'package:ynotes/ui/components/buttons.dart';
 import 'package:ynotes/ui/components/dialogs.dart';
 import 'package:ynotes/ui/components/modalBottomSheets/filesBottomSheet.dart';
