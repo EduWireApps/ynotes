9fb132754fdf07a5648277e255a84e2876f552fd
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-07-06 20:32:11 +0200

    finish last grades

--- lib/ui/screens/summary/temp/last_grades.dart
+++ lib/ui/screens/summary/temp/last_grades.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ynotes/core/logic/grades/controller.dart';
+import 'package:ynotes/core/logic/grades/models.dart';
+import 'package:ynotes/globals.dart';
+import 'package:ynotes/ui/screens/summary/temp/card.dart';
 import 'package:ynotes/ui/screens/summary/temp/constants.dart';
 import 'package:ynotes/ui/screens/summary/temp/texts.dart';
+import 'package:ynotes/useful_methods.dart';
 import 'package:ynotes_components/ynotes_components.dart';
+import 'package:sizer/sizer.dart';
 
 class SummaryLastGrades extends StatefulWidget {
   const SummaryLastGrades({Key? key}) : super(key: key);
@@ -11,29 +18,100 @@ class SummaryLastGrades extends StatefulWidget {
 }
 
 class _SummaryLastGradesState extends State<SummaryLastGrades> {
+  Widget gradeCard(BuildContext context, Grade grade) {
+    final TextStyle gradeStyle =
+        TextStyle(fontSize: 30, fontWeight: FontWeight.w600, color: currentTheme.colors.neutral.shade500);
+
+    return Padding(
+        padding: EdgeInsets.only(left: sidePadding),
+        child: GestureDetector(
+          onTap: () => Navigator.pushNamed(context, "/grades"),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 60.w),
+            child: SummaryCard(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text((grade.notSignificant! ? "(" + grade.value! : grade.value) ?? "", style: gradeStyle),
+                          Text('/' + grade.scale!,
+                              style: TextStyle(
+                                  color: currentTheme.colors.neutral.shade400,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400)),
+                          if (grade.notSignificant!) Text(")", style: gradeStyle)
+                        ],
+                      ),
+                      Expanded(child: Container()),
+                      Icon(Icons.arrow_forward_outlined, color: currentTheme.colors.neutral.shade400)
+                    ]),
+                YVerticalSpacer(5),
+                Text(
+                  grade.disciplineName ?? "",
+                  style:
+                      TextStyle(color: currentTheme.colors.neutral.shade400, fontSize: 12, fontWeight: FontWeight.w600),
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.left,
+                ),
+                Text(
+                  grade.testName ?? "",
+                  style:
+                      TextStyle(color: currentTheme.colors.neutral.shade500, fontSize: 18, fontWeight: FontWeight.w600),
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.left,
+                )
+              ],
+            )),
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: sidePadding),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(Texts.lastGrades,
-                style:
-                    TextStyle(color: currentTheme.colors.neutral.shade500, fontSize: 20, fontWeight: FontWeight.w500)),
-            YButton(
-              onPressed: () => Navigator.pushNamed(context, "/grades"),
-              text: "Tout voir",
-              type: YColor.neutral,
-              variant: YButtonVariant.reverse,
-              icon: Icons.arrow_forward_rounded,
-              reverseIconAndText: true,
-            )
-          ],
-        ),
-      )
-    ]);
+    return ChangeNotifierProvider<GradesController>.value(
+        value: appSys.gradesController,
+        child: Consumer<GradesController>(builder: (context, model, child) {
+          final allGrades =
+              getAllGrades(model.disciplines(showAll: true), overrideLimit: true, sortByWritingDate: true) ?? [];
+          final grades = allGrades.reversed.toList().sublist(allGrades.length - 5).reversed;
+          if (grades.length != 0) {
+            return Column(mainAxisSize: MainAxisSize.min, children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: sidePadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(Texts.lastGrades,
+                        style: TextStyle(
+                            color: currentTheme.colors.neutral.shade500, fontSize: 20, fontWeight: FontWeight.w500)),
+                    YButton(
+                      onPressed: () => Navigator.pushNamed(context, "/grades"),
+                      text: Texts.seeAll,
+                      type: YColor.neutral,
+                      variant: YButtonVariant.reverse,
+                      icon: Icons.arrow_forward_rounded,
+                      reverseIconAndText: true,
+                    )
+                  ],
+                ),
+              ),
+              YVerticalSpacer(10),
+              SingleChildScrollView(
+                padding: EdgeInsets.only(right: sidePadding),
+                scrollDirection: Axis.horizontal,
+                child: Row(children: grades.map((grade) => gradeCard(context, grade)).toList()),
+              ),
+            ]);
+          } else {
+            return Container();
+          }
+        }));
   }
 }
--- lib/ui/screens/summary/temp/texts.dart
+++ lib/ui/screens/summary/temp/texts.dart
@@ -1,6 +1,7 @@
 class Texts {
   static final String average = "de moyenne générale";
   static final String lastGrades = "Dernières notes";
+  static final String seeAll = "Tout voir";
 
   Texts._();
 }
