7174fe9805d97407075fc566632287d7bd874616
JsonLines <45465075+JsonLinesCode@users.noreply.github.com>
2021-10-16 23:34:19 +0200

    :lock: Add secure loger

--- lib/core/utils/logging_utils.dart
+++ lib/core/utils/logging_utils.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 
+import 'package:convert/convert.dart' as conv;
+import 'package:encrypt/encrypt.dart' as crypto;
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
+import 'package:uuid/uuid_util.dart';
+import 'package:ynotes/core/utils/kvs.dart';
 import 'package:ynotes/extensions.dart';
 
 import 'file_utils.dart';
@@ -53,5 +57,61 @@ class CustomLogger {
     final String currentText = await file.readAsString();
 
     await file.writeAsString(date + n + '[${object.toUpperCase()}] $text' + n + n + currentText, mode: FileMode.write);
+  }
+}
+
+class SecureLogger {
+  String? key;
+
+  Future<String> cipher(String text) async {
+    final _key = crypto.Key.fromUtf8(await getKey() ?? "");
+    final encrypter = crypto.Encrypter(crypto.AES(_key));
+    return encrypter.encrypt(text).base16;
+  }
+
+  Future<String> decipher(String text) async {
+    final _key = crypto.Key.fromUtf8(await getKey() ?? "");
+    final encrypter = crypto.Encrypter(crypto.AES(_key));
+    return encrypter.decrypt(crypto.Encrypted.fromBase16(text));
+  }
+
+  Future<String?> getKey() async {
+    if (key != null) {
+      return key;
+    }
+    if (await KVS.containsKey(key: "loggingKey")) {
+      key = await KVS.read(key: "loggingKey");
+      return key;
+    } else {
+      key = conv.hex.encode(UuidUtil.cryptoRNG());
+      if (key != null) {
+        await KVS.write(key: "loggingKey", value: key!);
+      }
+      return key;
+    }
+  }
+
+  Future<File> loadLog(String logName) async {
+    final directory = await FolderAppUtil.getDirectory();
+    String name = await cipher(logName);
+    final File file = File('${directory.path}/logs/$name.txt');
+    return file;
+  }
+
+  Future<String?> readLog({required String logName}) async {
+    File file = await loadLog(logName);
+    if ((await file.exists()) == false) {
+      await file.create();
+    }
+    return decipher(await file.readAsString());
+  }
+
+  Future<void> writeLog({required String logName, required String text}) async {
+    File file = await loadLog(logName);
+    if ((await file.exists()) == false) {
+      await file.create();
+    }
+
+    await file.writeAsString(await cipher(text), mode: FileMode.write);
   }
 }
