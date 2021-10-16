71b060c7ec636588efca64b2b88e8a6f01e68ba1
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-10-16 15:42:37 +0200

    update wiredash theme

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ import 'package:ynotes/backward_compatibility.dart';
 import 'package:ynotes/core/logic/app_config/controller.dart';
 import 'package:ynotes/core/services/background.dart';
 import 'package:ynotes/core/services/notifications.dart';
-import 'package:ynotes/core/utils/theme_utils.dart';
 import 'package:ynotes/core/utils/ui.dart';
 import 'package:ynotes/globals.dart';
 import 'package:ynotes/router.dart';
@@ -24,6 +23,7 @@ import 'package:ynotes/ui/screens/loading/loading.dart';
 import 'package:ynotes/ui/themes/themes.dart';
 import 'package:ynotes_packages/config.dart';
 import 'package:ynotes_packages/theme.dart';
+import 'package:ynotes_packages/utilities.dart';
 
 Future main() async {
   Logger.level = Level.warning;
@@ -80,15 +80,24 @@ class _AppState extends State<App> {
                     secret: "y9zengsvskpriizwniqxr6vxa1ka1n6u",
                     navigatorKey: _navigatorKey,
                     theme: WiredashThemeData(
-                        backgroundColor: ThemeUtils.isThemeDark ? const Color(0xff313131) : Colors.white,
-                        primaryBackgroundColor:
-                            ThemeUtils.isThemeDark ? const Color(0xff414141) : const Color(0xffF3F3F3),
-                        secondaryBackgroundColor: ThemeUtils.isThemeDark ? const Color(0xff313131) : Colors.white,
-                        secondaryColor: Theme.of(context).primaryColorDark,
-                        primaryColor: Theme.of(context).primaryColor,
-                        primaryTextColor: ThemeUtils.textColor(),
-                        brightness: Brightness.dark,
-                        secondaryTextColor: ThemeUtils.textColor().withOpacity(0.8)),
+                      backgroundColor: theme.colors.backgroundColor,
+                      primaryBackgroundColor: theme.colors.backgroundLightColor,
+                      secondaryBackgroundColor: theme.colors.backgroundColor,
+                      primaryColor: theme.colors.primary.backgroundColor,
+                      secondaryColor: theme.colors.primary.backgroundColor,
+                      primaryTextColor: theme.colors.foregroundColor,
+                      secondaryTextColor: theme.colors.foregroundLightColor,
+                      brightness: Brightness.dark,
+                      sheetBorderRadius: BorderRadius.vertical(top: Radius.circular(YScale.s6)),
+                      fontFamily: theme.fonts.primary,
+                      tertiaryTextColor: theme.colors.foregroundColor,
+                      dividerColor: theme.colors.primary.foregroundColor,
+                      errorColor: theme.colors.danger.backgroundColor,
+                      firstPenColor: theme.colors.info.backgroundColor,
+                      secondPenColor: theme.colors.success.backgroundColor,
+                      thirdPenColor: theme.colors.warning.backgroundColor,
+                      fourthPenColor: theme.colors.danger.backgroundColor,
+                    ),
                     options: WiredashOptionsData(
                       /// You can set your own locale to override device default (`window.locale` by default)
                       locale: const Locale.fromSubtags(languageCode: 'fr'),
