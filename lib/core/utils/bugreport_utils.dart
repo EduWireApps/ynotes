b342574931e2c7037c9c20db6b9e8af1c28a0f52
JsonLines <45465075+JsonLinesCode@users.noreply.github.com>
2021-10-28 21:44:30 +0200

    :art: Add schoolApi metadata field

--- lib/core/utils/bugreport_utils.dart
+++ lib/core/utils/bugreport_utils.dart
@@ -16,6 +16,8 @@ class BugReportUtils {
     initShakeToReport();
     Shake.setShowFloatingReportButton(false);
     Shake.setInvokeShakeOnScreenshot(false);
+
+    //Shake clients ids
     Shake.start('iGBaTEc4t0namXSCrwRJLihJPkMPnfco2z4Xoyi3', 'nfzb5JnoGoGVxEi75jejFhyTQL4MyyOC7yCMCYiOmKaykWdoh0kfbY8');
   }
 
@@ -28,17 +30,19 @@ class BugReportUtils {
     try {
       String json = jsonEncode(await CustomLogger.getAllLogs());
       final directory = await FolderAppUtil.getDirectory();
-
+      //create a temp file containing logs as json
       final File file = File('${directory.path}/logs/temp.json');
-
       if (await file.exists()) {
-        file.delete();
+        await file.delete();
       }
       file.create(recursive: true);
       await file.writeAsString(json);
       List<ShakeFile> shakeFiles = [];
       shakeFiles.add(ShakeFile.create(file.path, 'userLogs'));
+      //set shake report files
       Shake.setShakeReportData(shakeFiles);
+      //set api metadata
+      Shake.setMetadata("schoolApi", appSys.api?.apiName ?? "{undefined}");
     } catch (e) {
       CustomLogger.error(e);
     }
