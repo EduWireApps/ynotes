5b1635eec74c2b83a7b658d66adcb4979e526357
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-11-04 15:12:55 +0100

    add licenses page

--- lib/ui/components/NEW/dialogs/about_dialog.dart
+++ lib/ui/components/NEW/dialogs/about_dialog.dart
@@ -2,30 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:ynotes_packages/components.dart';
 import 'package:ynotes_packages/theme.dart';
-import 'package:ynotes_packages/utilities.dart';
 
 /// A dialog that handles legal stuff
 class AboutDialog extends StatelessWidget {
   const AboutDialog({Key? key}) : super(key: key);
-
-  Color get primary => theme.isDark ? theme.colors.primary.foregroundColor : theme.colors.primary.backgroundColor;
-
-  Color getColor(double opacity) {
-    return Color.fromRGBO(primary.red, primary.green, primary.blue, opacity);
-  }
-
-  MaterialColor get primarySwatch => MaterialColor(primary.value, {
-        50: getColor(.1),
-        100: getColor(.2),
-        200: getColor(.3),
-        300: getColor(.4),
-        400: getColor(.5),
-        500: getColor(.6),
-        600: getColor(.7),
-        700: getColor(.8),
-        800: getColor(.9),
-        900: getColor(1),
-      });
 
   /// A dialog that handles legal stuff
   @override
@@ -41,24 +21,7 @@ class AboutDialog extends StatelessWidget {
               body: Text(content, style: theme.texts.body1),
               actions: [
                 YButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) => Theme(
-                                  data: ThemeData(
-                                      primarySwatch: primarySwatch,
-                                      fontFamily: theme.fonts.primary,
-                                      brightness: theme.themeData.brightness),
-                                  child: LicensePage(
-                                      applicationIcon: Image(
-                                        image: const AssetImage('assets/appico/foreground.png'),
-                                        width: YScale.s12,
-                                      ),
-                                      applicationName: "yNotes",
-                                      applicationVersion: "${snapshot.data!.version} ${snapshot.data!.buildNumber}",
-                                      applicationLegalese: content))));
-                    },
+                    onPressed: () => Navigator.pushNamed(context, "/settings/licenses"),
                     text: "LICENSES",
                     variant: YButtonVariant.text),
                 YButton(onPressed: () => Navigator.pop(context), text: "FERMER")
--- lib/ui/screens/settings/routes.dart
+++ lib/ui/screens/settings/routes.dart
@@ -1,6 +1,7 @@
 import 'package:ynotes/router.dart';
 import 'package:ynotes/ui/screens/settings/settings_tmp.dart';
 import 'package:ynotes/ui/screens/settings/sub_pages/account.dart';
+import 'package:ynotes/ui/screens/settings/sub_pages/licenses.dart';
 import 'package:ynotes/ui/screens/settings/sub_pages/notifications.dart';
 import 'package:ynotes/ui/screens/settings/sub_pages/support.dart';
 
@@ -10,4 +11,5 @@ final List<CustomRoute> settingsRoutes = [
   CustomRoute(path: "/settings/account", page: const SettingsAccountPage(), relatedApi: -1, show: false),
   CustomRoute(path: "/settings/notifications", page: const SettingsNotificationsPage(), relatedApi: -1, show: false),
   CustomRoute(path: "/settings/support", page: const SettingsSupportPage(), relatedApi: -1, show: false),
+  CustomRoute(path: "/settings/licenses", page: const SettingsLicensesPage(), relatedApi: -1, show: false),
 ];
