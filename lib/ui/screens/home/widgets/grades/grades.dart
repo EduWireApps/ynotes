2398929f6ddb62a16e91bbed70b170db14036fd8
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-11-25 19:45:35 +0100

    fix average calculation

--- lib/ui/screens/home/widgets/grades/grades.dart
+++ lib/ui/screens/home/widgets/grades/grades.dart
@@ -39,7 +39,7 @@ class _GradesSectionState extends State<GradesSection> {
   double calculateGlobalAverage(List<Grade> grades) {
     // Will keep the averages of the disciplines
     final List<double> avgs = [];
-    grades = grades.where((grade) => double.tryParse(grade.value!) != null).toList();
+    grades = grades.where((grade) => double.tryParse(grade.value!.replaceAll(",", ".")) != null).toList();
     // We iterate through each discipline
     for (final discipline in controller.disciplines()!) {
       // We get the grades for the current discipline
