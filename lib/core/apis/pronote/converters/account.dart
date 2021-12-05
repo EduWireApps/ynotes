2b1518d6e9e5f05052a20ef39d5927cb4ad59a6a
JsonLines <45465075+JsonLinesCode@users.noreply.github.com>
2021-12-05 17:59:55 +0100

    :sparkles: Add a new grade calculation method

--- lib/core/apis/pronote/converters/account.dart
+++ lib/core/apis/pronote/converters/account.dart
@@ -57,6 +57,8 @@ class PronoteAccountConverter {
         });
       });
     }
+    tabsNumbers = tabsNumbers.toSet().toList();
+    tabsNumbers.sort();
     CustomLogger.log("ACCOUNT", "Tabs numbers: $tabsNumbers");
     for (var element in (hiddenTabs ?? [])) {
       tabsNumbers.remove(element);
