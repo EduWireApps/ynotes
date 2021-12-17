c7296e4169406d2714bf8c68f1220795b3596448
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-12-17 18:27:45 +0100

    document email model

--- lib/packages/school_api_models/src/email.dart
+++ lib/packages/school_api_models/src/email.dart
@@ -1,22 +1,47 @@
 part of models;
 
+/// The model for an email.
+///
+/// Can be stored in [Hive] storage.
 @HiveType(typeId: _HiveTypeIds.email)
 class Email {
+  /// The id of the email.
   @HiveField(0)
   final String id;
+
+  /// Is the email read. Don't forget to update the [Email]
+  /// in the [EmailsModule] if you change it.
   @HiveField(1)
   bool read;
+
+  /// The email's sender.
   @HiveField(2)
   final Recipient from;
+
+  /// The email's subject.
   @HiveField(3)
   final String subject;
+
+  /// The date the e-mail was sent.
   @HiveField(4)
   final DateTime date;
+
+  /// The email's content. By default when querying the emails' list
+  /// from the api, it's likely to be null. You have to call the api
+  /// again to get all emails' data. Don't forget to update the [Email]
+  /// in the [EmailsModule].
   @HiveField(5)
   String? content;
+
+  /// The email's documents ids, used to retrieve linked documents
+  /// in [documents] from [DocumentsModule].
   @HiveField(6)
   final List<String> documentsIds;
+
+  /// Get the email's documents from a list of [Document]s.
   List<Document> documents(List<Document> d) => d.where((document) => documentsIds.contains(document.id)).toList();
+
+  /// The email's recipients.
   @HiveField(7)
   final List<Recipient> to;
 
@@ -31,6 +56,9 @@ class Email {
     required this.to,
   });
 
+  /// When sending an [Email], some data are useless and so
+  /// this factory let's you create an [Email] with only the
+  /// required data.
   factory Email.toSend(
           {required String subject,
           required String content,
