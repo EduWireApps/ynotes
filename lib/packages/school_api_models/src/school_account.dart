201487359013900ac8b9130e0b7638147825fdcb
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-12-19 20:04:25 +0100

    document school account model

--- lib/packages/school_api_models/src/school_account.dart
+++ lib/packages/school_api_models/src/school_account.dart
@@ -1,11 +1,26 @@
 part of models;
 
+/// The model for the school account. This account is used to
+/// retrive data.
+///
+/// Can't be stored in [Hive] storage for now.
 class SchoolAccount {
-  final String firstName;
-  final String lastName;
-  String get fullName => '$firstName $lastName';
-  final String className;
+  /// The account's id.
   final String id;
+
+  /// The first name of the user.
+  final String firstName;
+
+  /// The last name of the user.
+  final String lastName;
+
+  /// The full name of the user, computed from [firstName] and [lastName].
+  String get fullName => '$firstName $lastName';
+
+  /// The name of the user's class.
+  final String className;
+
+  /// The user's profile picture url.
   final String profilePicture;
 
   const SchoolAccount(
