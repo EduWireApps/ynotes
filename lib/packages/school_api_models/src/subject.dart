23acee511eeea3db124afd110be4c09d06a6e7a6
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-12-20 21:21:58 +0100

    document grade model

--- lib/packages/school_api_models/src/subject.dart
+++ lib/packages/school_api_models/src/subject.dart
@@ -1,23 +1,43 @@
 part of models;
 
+/// The model for a subject.
+///
+/// Can be stored in [Hive] storage.
 @HiveType(typeId: _HiveTypeIds.subject)
 class Subject {
+  /// The id of the subject.
   @HiveField(0)
   final String id;
+
+  /// The name of the subject.
   @HiveField(1)
   final String name;
+
+  /// The average of the class for this subject.
   @HiveField(2)
   final double classAverage;
+
+  /// The maximum average of the class for this subject.
   @HiveField(3)
   final double maxAverage;
+
+  /// The minimum average of the class for this subject.
   @HiveField(4)
   final double minAverage;
+
+  /// The coefficient of the subject.
   @HiveField(5)
   final double coefficient;
+
+  /// The teachers of the subject.
   @HiveField(6)
   final String teachers;
+
+  /// The grades of the subject.
   List<Grade> grades(List<Grade> grades, [Period? period]) =>
       grades.where((g) => g.subjectId == id && (period == null ? true : g.periodId == period.id)).toList();
+
+  /// The average of the subject.
   @HiveField(7)
   final double average;
 
@@ -32,6 +52,14 @@ class Subject {
       required this.average});
 }
 
+/// The model for a custom [Grade].
+///
+/// It only requires required data and can be used to
+/// sort between [Grade]s and [CustomGrade]s:
+///
+/// ```dart
+/// final bool custom = grade is CustomGrade;
+/// ```
 class CustomSubject extends Subject {
   CustomSubject(
       {required String name,
