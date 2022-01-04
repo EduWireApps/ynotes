668e33efcde3814ef21fc0658ae17c8ffc855602
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-01-04 21:57:29 +0100

    uncomment condition

--- lib/ui/screens/grades_new/widgets/period_page.dart
+++ lib/ui/screens/grades_new/widgets/period_page.dart
@@ -129,21 +129,21 @@ class _Stats extends StatelessWidget {
               ),
               YHorizontalSpacer(YScale.s4),
               Expanded(child: Container()),
-              // if (average != period.overallAverage)
-              YIconButton(
-                icon: Icons.info_rounded,
-                onPressed: () async {
-                  await YDialogs.showInfo(
-                      context,
-                      YInfoDialog(
-                          title: "Données obsolètes",
-                          confirmLabel: "OK",
-                          body: Text(
-                              "Seule la moyenne générale est calculée en temps réel. Actuellement, les autres données (moyenne de classe, maximum et minimum) sont obsolètes, elles seront mises à jour dès que l'information sera disponible.",
-                              style: theme.texts.body1)));
-                },
-                foregroundColor: theme.colors.warning.backgroundColor,
-              ),
+              if (average != period.overallAverage)
+                YIconButton(
+                  icon: Icons.info_rounded,
+                  onPressed: () async {
+                    await YDialogs.showInfo(
+                        context,
+                        YInfoDialog(
+                            title: "Données obsolètes",
+                            confirmLabel: "OK",
+                            body: Text(
+                                "Seule la moyenne générale est calculée en temps réel. Actuellement, les autres données (moyenne de classe, maximum et minimum) sont obsolètes, elles seront mises à jour dès que l'information sera disponible.",
+                                style: theme.texts.body1)));
+                  },
+                  foregroundColor: theme.colors.warning.backgroundColor,
+                ),
               YHorizontalSpacer(YScale.s1),
               YButton(
                 onPressed: _open,
