b5e6407998c1f4cb845ed1c08a9253cfb4dc29fd
JsonLines <45465075+JsonLinesCode@users.noreply.github.com>
2022-03-03 20:21:21 +0100

    :green_heart: Update other CI metas

--- .github/scripts/decrypt_ios_keys.sh
+++ .github/scripts/decrypt_ios_keys.sh
@@ -3,5 +3,5 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
---output ios/AuthKey_5R2MFY6PX4.p8 ios/AuthKey_5R2MFY6PX4.p8.gpg
+--output ios/AuthKey_YM9RAPWYT7.p8 ios/AuthKey_YM9RAPWYT7.p8.gpg
 
